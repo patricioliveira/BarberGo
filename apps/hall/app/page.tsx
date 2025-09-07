487d473e74355f6c76d2c8e22d917eb73eb0ebe9
Patricio Oliveira <77838764+patricioliveira@users.noreply.github.com>
2025-09-07 13:14:49 -0300

    Updated page.tsx

--- apps/hall/app/page.tsx
+++ apps/hall/app/page.tsx
@@ -1,28 +1,24 @@
-@@ .. @@
--import Header from "./_components/header"
--import { Button } from "./_components/ui/button"
-+import Header from "./_components/header"
-+import { Button } from "@barbergo/ui"
- import Image from "next/image"
--import { db } from "./_lib/prisma"
-+import { db } from "@barbergo/database"
- import BarbershopItem from "./_components/barbershop-item"
- import { quickSearchOptions } from "./_constants/search"
- import BookingItem from "./_components/booking-item"
-@@ .. @@
- const Home = async () => {
-   const session = await getServerSession(authOptions)
--  const barbershops = await db.barbershop.findMany({})
-+  const barbershops = await db.barbershop.findMany({
-+    where: {
-+      isExclusive: false,
-+    },
-+  })
-   const popularBarbershops = await db.barbershop.findMany({
-+    where: {
-+      isExclusive: false,
-+    },
-     orderBy: {
-       name: "desc",
-     },
-   })
+import Header from "./_components/header"
+import { Button } from "@barbergo/ui"
+import Image from "next/image"
+import { db } from "@barbergo/database"
+import BarbershopItem from "./_components/barbershop-item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
+
+const Home = async () => {
+  const session = await getServerSession(authOptions)
+  const barbershops = await db.barbershop.findMany({
+    where: {
+      isExclusive: false,
+    },
+  })
+  const popularBarbershops = await db.barbershop.findMany({
+    where: {
+      isExclusive: false,
+    },
+    orderBy: {
+      name: "desc",
+    },
+  })
+}
